c1e44b2c58f136f7a9c808e8b8b0f8907cfb1e82
SilverFlame83 <imren_83@abv.bg>
2022-12-01 12:32:38 +0200

    Portals implemented

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,12 +1,16 @@
 import { React, Fragment } from "react";
+import  ReactDOM from "react-dom";
 
 import styles from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
 
-const ErrorModal = (props) => {
+const BackDrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onError}></div>;
+};
+
+const LayerDrop = (props) => {
   return (
-    <Fragment className={styles.backdrop } onClick={props.onError}>
     <Card className={styles.modal}>
       <header className={styles.header}>
         <h2>{props.title}</h2>
@@ -18,6 +22,24 @@ const ErrorModal = (props) => {
         <Button onClick={props.onError}>Okay</Button>
       </footer>
     </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onError={props.onError} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <LayerDrop
+          title={props.title}
+          message={props.message}
+          onError={props.onError}
+        />,
+        document.getElementById('layout-root')
+      )}
     </Fragment>
   );
 };
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,9 +1,10 @@
 import React, { useState, Fragment } from "react";
-import Button from "../UI/Button";
 
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUser.module.css";
+
 
 const AddUser = (props) => {
   const [enteredName, setEnteredName] = useState("");
@@ -44,6 +45,7 @@ const AddUser = (props) => {
   const errorHandler = () => {
     setModalError(null);
   };
+
 
   return (
     <Fragment>
